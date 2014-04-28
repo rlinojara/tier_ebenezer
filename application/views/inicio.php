1212db726b63eae1dbadbb6865e890ab6b449254
Gianino <g24863179x>
2014-04-28 16:38:25 -0500

    Cambio de idioma inicio.php

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,16 +1,15 @@
- <aside class="right-side">
-                <section class="content-header">
-                    <h1>
-                        Dashboard
-                        <small>Control panel</small>
-                    </h1>
-                    <ol class="breadcrumb">
-                        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                        <li class="active">Dashboard</li>
-                    </ol>
-                </section>
-                <section class="content">
-                    <div class="row">
-                    </div>             
-               	</section>
-            </aside>
+<aside class="right-side">
+    <section class="content-header">
+        <h1>
+            Panel principal
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        </ol>
+    </section>
+    <section class="content">
+        <div class="row">
+            
+        </div>
+    </section>
+</aside>
