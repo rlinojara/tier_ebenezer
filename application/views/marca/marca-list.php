b3310b21399563ddb98c7edcac24e111a480e98c
Gianino <cole.gianino@gmail.com>
2014-05-02 11:58:23 -0500

    marca vistra lsitado

--- application/views/marca/marca-list.php
+++ application/views/marca/marca-list.php
@@ -1,7 +1,7 @@
 <aside class="right-side">
     <section class="content-header">
         <h1>
-            Listado de productos
+            Listado de marcas
             <small>Control panel</small>
         </h1>
         <ol class="breadcrumb">
@@ -14,7 +14,7 @@
 	        <div class="col-md-12">
                 <div id="superiorList">
                     <div class="btn-group-vertical">
-                        <?php echo anchor('producto/registrar_producto','Agregar','class="btn btn-primary btn-flat"'); ?>
+                        <?php echo anchor('marca/registrar_marca','Agregar','class="btn btn-primary btn-flat"'); ?>
                     </div>
                 </div>
 				<div class="box">
@@ -25,21 +25,21 @@
                             <tbody>
                             	<tr>
 	                                <th style="width: 10px">#</th>
-	                                <th>Producto</th>
-	                                <th>Marca</th>
-	                                <th>Medida</th>
-	                                <th>Modelo</th>
+	                                <th>marca</th>
+	                                <th>Nombres</th>
+	                                <th>Apellidos</th>
+	                                <th>Estados</th>
 	                                <th style="width: 160px">Acciones</th>
                             	</tr>
-                            <?php for($i = 0 ; $i < count($productos); $i++):?>
+                            <?php for($i = 0 ; $i < count($marcas); $i++):?>
                             <tr>
                              	<td>1.</td>
-                                <td><?php echo $productos[$i]['producto']?></td>
-                                <td><?php echo $productos[$i]['marca']?></td>
-                                <td><?php echo $productos[$i]['medida']?></td>
+                                <td><?php echo $marcas[$i]['marca']?></td>
+                                <td><?php echo $marcas[$i]['nombre']?></td>
+                                <td><?php echo $marcas[$i]['apellido']?></td>
                                 <td>
                                     <?php 
-                                        if($productos[$i]['estado'] == 1):
+                                        if($marcas[$i]['estado'] == 1):
                                             echo '<span class="btn btn-success disabled">Activo</span>';
                                         else:
                                             echo '<span class="btn btn-danger disabled">Inactivo</span>';
@@ -48,11 +48,11 @@
                                 </td>
                                 <td>
                                     <?php 
-                                    echo anchor('producto/registrar_producto/'.$productos[$i]['id_producto'],'Editar','class="btn btn-info btn-sm"');
-                                    if($productos[$i]['estado'] == 1):
-                                        echo anchor('producto/deshabilitar_producto/'.$productos[$i]['id_producto'],'Deshabilitar','class="btn btn-danger btn-sm"'); 
+                                    echo anchor('marca/registrar_marca/'.$marcas[$i]['id_marca'],'Editar','class="btn btn-info btn-sm"');
+                                    if($marcas[$i]['estado'] == 1):
+                                        echo anchor('marca/deshabilitar_marca/'.$marcas[$i]['id_marca'].'/'.$pagina,'Deshabilitar','class="btn btn-danger btn-sm"'); 
                                     else:
-                                        echo anchor('producto/habilitar_producto/'.$productos[$i]['id_producto'],'Habilitar','class="btn btn-success btn-sm"');
+                                        echo anchor('marca/habilitar_marca/'.$marcas[$i]['id_marca'].'/'.$pagina,'Habilitar','class="btn btn-success btn-sm"');
                                     endif;
                                     ?>
 
